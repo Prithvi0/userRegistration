c286872d9efb4b4b6ff3697503763efb1876f533
prithvi0 <prithviy96@gmail.com>
2020-03-18 18:50:25 +0530

    Added program to valid last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,9 +3,16 @@ printf "Welcome to User Registration\n"
 # VALID PATTERN FOR FIRST LETTER CAPITAL AND LENGTH OF ATLEAST 3 CHARACTERS
 pattern="^[A-Z]{1}[a-z]{2,}$"
 read -p "Enter your first name: " firstName
-if [[ "$firstName" =~ $pattern ]]
+read -p "Enter your last name: " lastName
+if [[ $firstName =~ $pattern && $lastName =~ $pattern ]]
 then
-	echo "true"
+	printf "Both first name and last name are valid\n"
+elif [[ $firstName =~ $pattern && ! $lastName =~ $pattern ]]
+then
+	printf "Only first name is valid\n"
+elif [[ ! $firstName =~ $pattern && $lastName =~ $pattern ]]
+then
+	printf "Only last name is valid\n"
 else
-	echo "false"
+	printf "Both first name and last name are invalid\n"
 fi
