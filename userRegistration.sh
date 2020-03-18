1174d26c3a63f90750f44be38f9a383e898d512c
prithvi0 <prithviy96@gmail.com>
2020-03-18 18:33:02 +0530

    Added program to valid first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
 printf "Welcome to User Registration"
+# VALID PATTERN FOR FIRST LETTER CAPITAL AND LENGTH OF ATLEAST 3 CHARACTERS
+pattern="^[A-Z]{1}[a-z]{2,}$"
+read -p "Enter your first name: " firstName
+if [[ "$firstName" =~ "$firstName" ]]
+then
+	echo "true"
+else
+	echo "false"
+fi
