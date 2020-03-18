02747f30523f69b61d021daf8b5d0feff3dfb735
prithvi0 <prithviy96@gmail.com>
2020-03-18 18:37:43 +0530

    Modified the if condition in the program

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
-printf "Welcome to User Registration"
+printf "Welcome to User Registration\n"
 # VALID PATTERN FOR FIRST LETTER CAPITAL AND LENGTH OF ATLEAST 3 CHARACTERS
 pattern="^[A-Z]{1}[a-z]{2,}$"
 read -p "Enter your first name: " firstName
-if [[ "$firstName" =~ "$firstName" ]]
+if [[ "$firstName" =~ $pattern ]]
 then
 	echo "true"
 else
